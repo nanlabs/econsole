49d9f5f8816dbbc5848af632f801a51a052ec51c
Martin Moscovich <martinmoscovich@gmail.com>
2013-08-06 12:05:16 -0300

    initial commit of code

---
+++ index.js
@@ -0,0 +1,204 @@
+/**
+ * Console Enhancer
+ * Provides new features to console, such as styling, source information (file and line number) and level-based filtering
+ *
+ * @author Martin Moscovich (martin@nan-labs.com)
+ * Copyright (c) 2013 NaN Labs (www.nan-labs.com)
+ */
+
+
+var util = require("util");
+
+/**
+ * ERROR LEVELS. 
+ * Higher Levels include the lower levels.
+ */
+var levels = {
+	ERROR: 0,
+	WARN: 1,
+	INFO: 2,
+	DEBUG: 3,
+	TRACE: 4,
+	ALL: 5
+};
+
+/**
+ * Font styles
+ */
+var styles = {
+	CLEAR: 0,
+	BOLD: 1,
+	ITALIC: 3,
+	UNDERLINE: 4
+};
+
+/**
+ * Font colors
+ */
+var colors = {
+	RED: 31,
+	YELLOW: 33,
+	WHITE: 37,
+	BLACK: 30
+};
+
+/**
+ * Definitions of styles and colors for each level
+ */
+var levelStyles = {
+	ERROR: [styles.BOLD, colors.RED],
+	WARN: [styles.BOLD, colors.YELLOW],
+	INFO: [styles.BOLD, colors.WHITE],
+	DEBUG: [colors.WHITE],
+	TRACE: [styles.BOLD, colors.BLACK]
+};
+
+/**
+ * Current level set
+ */
+var currentLevel = levels.ALL;
+
+/**
+ * Flag indicating whether to show the file name and line on each log.
+ * Currently, it is always true except when level is ERROR 
+ * (to improve performance and because the error's stack contains the line number)
+ */
+ var showSourceInfo = true;
+
+/**
+ * Function to call in order to improve node's console.
+ * @param level the log level
+ */
+exports.enhance = function(level) {
+	if(level === 'VERBOSE') level = 'TRACE';
+
+	var levelNumber = levels[level];
+	if (typeof levelNumber !== 'undefined') currentLevel = levelNumber;
+	
+	showSourceInfo = (currentLevel !== 0);
+
+	global.console.info = LogBuilder.createLogger("INFO");
+
+	global.console.warn = LogBuilder.createLogger("WARN");
+
+	global.console.debug = LogBuilder.createLogger("DEBUG");
+
+	global.console.verbose = LogBuilder.createLogger("TRACE");
+
+	global.console.error = LogBuilder.createErrorLogger();
+
+	global.console.log = global.console.debug;
+};
+
+/**
+ * Writes the log to stderr
+ *
+ * Partially based on clim (http://github.com/epeli/node-clim)
+ * Copyright (c) 2009-2011 Esa-Matti Suuronen <esa-matti@suuronen.org>
+ */
+function writeLog(level, msg) {
+	var line;
+	if(showSourceInfo) {
+		var si = getSourceInfo();
+		//if(si.methodName === 'anonymous') {
+		line = util.format("<%s>\t[%s:%s] %s", level, si.file, si.lineNumber, msg);
+		//} else {
+		//	line = util.format("<%s>\t[%s:%s - %s()] %s", level, si.file, si.lineNumber, si.methodName, msg);
+		//}
+	} else {
+		line = util.format("<%s>\t%s", level, msg);
+	}
+	
+	line = logStyler.addCodes(line, levelStyles[level]);
+	process.stderr.write(line + "\n");
+}
+
+/**
+ * Gets the source information from the line being log
+ */
+function getSourceInfo() {
+	var exec = getStack()[3];
+
+	var pos = exec.getFileName().lastIndexOf('\\');
+	if(pos < 0) exec.getFileName().lastIndexOf('/');
+
+	return {
+		methodName: exec.getFunctionName() || 'anonymous',
+		file: exec.getFileName().substring(pos + 1),
+		lineNumber: exec.getLineNumber()
+	};
+}
+
+
+/**
+ * Gets the current stack in order to retrieve source code info.
+ *
+ * Based on Callsite (http://github.com/visionmedia/callsite)
+ * Copyright (c) 2011, 2013 TJ Holowaychuk <tj@vision-media.ca>
+ */
+function getStack() {
+	var old = Error.stackTraceLimit;
+	Error.stackTraceLimit = 4;
+	var orig = Error.prepareStackTrace;
+  	Error.prepareStackTrace = function(_, stack){ return stack; };
+  	var err = new Error;
+  	Error.captureStackTrace(err, arguments.callee);
+  	var stack = err.stack;
+  	Error.prepareStackTrace = orig;
+
+	Error.stackTraceLimit = old;
+
+  	return stack;
+}
+
+
+/**
+ * Creates the loggers
+ */
+var LogBuilder = {
+	createLogger: function(level) {
+	  return function () {
+	  	if(levels[level] <= currentLevel) {
+		  	var msg = util.format.apply(this, arguments);
+		  	writeLog(level, msg);
+	  	}
+	  };
+	},
+
+	createErrorLogger: function() {
+		var level = "ERROR";
+		return function () {
+			if(levels[level] <= currentLevel) {
+
+		  		if(arguments[0] instanceof Error) {
+		  			arguments[0] = arguments[0].stack; 
+		  		} else if(arguments[1] instanceof Error) {
+		  			arguments = [arguments[0] + ":\n" + arguments[1].stack];
+				}
+
+			  	var msg = util.format.apply(this, arguments);
+			  	writeLog(level, msg);
+		  	}
+		};
+	}
+}
+
+/**
+ * Adds the styles to the console logs
+ */
+var logStyler = {
+	getAnsi: function(style) {
+		return '\u001b['+style+'m';
+	},
+
+	addCodes: function(msg, codes) {
+		codes.forEach(function(c) {
+			msg = logStyler.getAnsi(c) + msg;
+		});
+
+		msg = msg + logStyler.getAnsi(styles.CLEAR);
+
+		return msg;
+	}
+}
+
---
+++ test.js
@@ -0,0 +1,61 @@
+/**
+ * Console Enhancer Sample
+ * 
+ * Execute using "node test.js <LOG LEVEL>" (eg "node test.js WARN" or "node test.js ALL")
+ * If no level is passed, ALL is used.
+ */
+
+// Dependency
+var econsole = require('./index')
+
+// Retrieve log level from command line parameter
+var logLevel = (process.argv.length>2)?process.argv[2]:'ALL';
+
+// First, show how regular console shows this
+console.log('\n\nRegular console\n');
+console.log('---------------');
+writeLogs(false);
+
+// Enhance console and call the method again to show the difference
+console.log('\n\nEnhanced console with log level', logLevel);
+console.log('--------------------------------------');
+
+econsole.enhance(logLevel)
+writeLogs(true);
+
+/**
+ * Writes logs for the different levels
+ * @param customMethods if false, only standard node's console methods will be called.
+ *						if true, besides standard methods, also the 2 new added methods are called
+ */
+function writeLogs(customMethods) {
+
+	// Error level logging with a message but no error
+	console.error('Testing ERROR LEVEL without actual error')
+	
+	// Error level logging with an error but no message
+	// This shows enhanced error parsing in logging 
+	console.error(new Error("some error"));
+
+	// Error level logging with both message and error
+	// This shows enhanced error parsing in logging 
+	console.error('Testing ERROR LEVEL with an error', new Error("some error"));
+	
+	// Warn level logging
+	console.warn('Testing WARN LEVEL');
+
+	// Info level logging
+	console.info('Testing INFO LEVEL');
+
+	// Log/Debug level logging
+	console.log('Testing LOG LEVEL');	
+	
+	if(customMethods) {
+		// Log/Debug level loggin using "debug" alias (not standard but clearer)
+		console.debug('Testing DEBUG (AKA LOG) LEVEL');
+		
+		// Verbose level logging (new level added to go beyond DEBUG)
+		console.verbose('Testing TRACE LEVEL');	
+	}
+}
+
